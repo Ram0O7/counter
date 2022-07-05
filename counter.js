bef36ebf55079a32f4537a5522c2f44b16b13578
Ramkrishn <ram706860@gmail.com>
2022-07-05 11:15:41 +0530

    audioadded

--- counter.js
+++ counter.js
@@ -1,6 +1,10 @@
 // set initial count
 let count = 0;
 
+let audio1 = new Audio("audio1.wav");
+let audio2 = new Audio("audio2.wav");
+let audio3 = new Audio("audio3.wav");
+
 //select value and buttons
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
@@ -9,12 +13,15 @@ btns.forEach(function (btn) {
     btn.addEventListener("click", function (e) {
         const styles = e.currentTarget.classList;
         if (styles.contains("decrease")) {
+            audio3.play();
             count--;
         }
         else if (styles.contains("increase")) {
+            audio1.play();
             count++;
         }
         else {
+            audio2.play();
             count = 0;
         }
         if (count > 0) {
